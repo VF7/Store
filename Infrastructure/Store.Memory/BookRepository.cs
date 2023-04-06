554fb12a7a418b2d66bce31b4d63e63cf7379eea
Viktor <fvi7707@gmail.com>
2023-04-06 04:58:53 +0300

    add Web app

---
+++ Infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]{
+            new Book(1, "Book Programming"),
+            new Book(2, "Book Designers"),
+            new Book(3, "Book Refactoring"),
+            };
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book=>book.Title.Contains(titlePart)).ToArray();
+        }
+    }
+}
